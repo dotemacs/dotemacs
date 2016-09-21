6444025ce9069647e27992d2efc9b65f2ced6e75
Александар Симић <asimic@gmail.com>
2016-09-21 09:51:52 +0100

    Addex xml-pretty-print function
    Useful to have around for those XML adventures

--- init.el
+++ init.el
@@ -188,6 +188,26 @@
   (setq desktop-dirname "~/.emacs.d")
   (desktop-save-mode t))
 
+(use-package nxml-mode
+  :config
+  (defun xml-pretty-print (begin end)
+    "Pretty format XML markup in region. You need to have nxml-mode
+    http://www.emacswiki.org/cgi-bin/wiki/NxmlMode installed to do
+    this.  The function inserts linebreaks to separate tags that have
+    nothing but whitespace between them.  It then indents the markup
+    by using nxml's indentation rules.
+
+    Original author Benjamin Ferrari:
+    http://blog.bookworm.at/2007/03/pretty-print-xml-with-emacs.html"
+    (interactive "r")
+    (save-excursion
+      (nxml-mode)
+      (goto-char begin)
+      (while (search-forward-regexp "\>[ \\t]*\<" nil t)
+        (backward-char) (insert "\n"))
+      (indent-region begin end))
+    (message "Ah, much better!")))
+
 (defun tmpbuf (buf)
     "open a buffer,
   if it doesn't exist, open a new one"
